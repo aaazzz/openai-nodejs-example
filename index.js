7cf36519320058e36a55508f191e1c0dba742819
aaazzz <akrm@hey.com>
2023-03-16 18:43:28 +0700

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const { Configuration, OpenAIApi } = require('openai');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+const messages = [
+  { role: 'user', content: 'Hello, my name is Akira. What your name?' },
+];
+
+(async () => {
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+  });
+  const completionText = completion.data.choices[0].message.content;
+  console.log(completionText);
+})();
